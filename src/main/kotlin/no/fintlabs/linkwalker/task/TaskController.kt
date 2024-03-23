097ebb05ed73eea9424003f67c9cd05e62e56b97
Hknots <hknots99@gmail.com>
2024-03-23 14:45:55 +0100

    Add TaskController

---
+++ src/main/kotlin/no/fintlabs/linkwalker/task/TaskController.kt
@@ -0,0 +1,21 @@
+package no.fintlabs.linkwalker.task
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/tasks/{organisation}")
+class TaskController(val taskService: TaskService) {
+
+    @PostMapping
+    fun postLink(@RequestBody task: Task,
+                 @PathVariable organisation: String,
+                 @RequestHeader("Authorization") authToken: String?): ResponseEntity<Any> {
+        if (authToken == null || task.clientName == null) {
+            return ResponseEntity.badRequest().body("Auth header or client name must be set")
+        }
+        taskService.startTask(task, organisation, authToken)
+        return ResponseEntity.accepted().body(task)
+    }
+
+}
