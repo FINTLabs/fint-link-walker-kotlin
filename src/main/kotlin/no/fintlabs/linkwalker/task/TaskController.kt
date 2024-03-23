4b7cb568ad3334f0c70dca7fcf57bb8f2b0da1a4
Hknots <hknots99@gmail.com>
2024-03-23 15:00:57 +0100

    require x-org-id in header

--- src/main/kotlin/no/fintlabs/linkwalker/task/TaskController.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/task/TaskController.kt
@@ -4,15 +4,15 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/tasks/{organisation}")
+@RequestMapping("/tasks")
 class TaskController(val taskService: TaskService) {
 
     @PostMapping
-    fun postLink(@RequestBody task: Task,
-                 @PathVariable organisation: String,
+    fun postTask(@RequestBody task: Task,
+                 @RequestHeader("x-org-id", required = true) organisation: String,
                  @RequestHeader("Authorization") authToken: String?): ResponseEntity<Any> {
         if (authToken == null || task.clientName == null) {
-            return ResponseEntity.badRequest().body("Auth header or client name must be set")
+            return ResponseEntity.badRequest().body("Auth header or client name in body must be set")
         }
         taskService.startTask(task, organisation, authToken)
         return ResponseEntity.accepted().body(task)
