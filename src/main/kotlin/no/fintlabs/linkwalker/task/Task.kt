99e027b5bff00393d15a03a67c815646ccd6f989
Hknots <hknots99@gmail.com>
2024-03-23 15:25:24 +0100

    ignore null elements & token in JSON

--- src/main/kotlin/no/fintlabs/linkwalker/task/Task.kt
+++ src/main/kotlin/no/fintlabs/linkwalker/task/Task.kt
@@ -1,11 +1,15 @@
 package no.fintlabs.linkwalker.task
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties("token")
 class Task(val url: String, var token: String? = null, var clientName: String? = null, var filter: List<String>? = null) {
-    var org: String? = null
     var id: String = UUID.randomUUID().toString()
+    var org: String? = null
     var status: Status = Status.STARTED
     var requests: AtomicInteger = AtomicInteger(0)
 }
