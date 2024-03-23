2c0c8e38661db9fecd0efeabc1c5cd4c92ecc31d
Hknots <hknots99@gmail.com>
2024-03-23 14:45:32 +0100

    Add LinkWalker

---
+++ src/main/kotlin/no/fintlabs/linkwalker/LinkWalker.kt
@@ -0,0 +1,17 @@
+package no.fintlabs.linkwalker
+
+import no.fintlabs.linkwalker.task.Task
+import org.springframework.stereotype.Service
+
+@Service
+class LinkWalker {
+
+    fun processTaskWithToken(task: Task) {
+
+    }
+
+    fun processTaskWithClientName(task: Task) {
+
+    }
+
+}
